0ae271060ab9c912f88abfe4e2fa04f2883d1c00
dev-nayandas <rich.nayan@gmail.com>
2022-08-22 02:30:42 +0600

    calculation start

--- calculation.js
+++ calculation.js
@@ -1,1 +1,23 @@
-console.log('Hello world')
+// function getInputFieldValueById(inputFieldId) {
+//     const inputField = document.getElementById(inputFieldId);
+//     const inputFieldValueString = inputField.value;
+//     const inputFieldValue = parseFloat(inputFieldValueString);
+//     inputField.value = '';
+//     return inputFieldValue;
+// }
+
+
+
+
+document.getElementById('calculationButton').addEventListener('click', function(){
+
+   
+    const perPlayerCost = document.getElementById('perPlayerCost').value;
+    const perPlayerCostValue = parseFloat(perPlayerCost);
+    const totalPlayerCost = perPlayerCostValue * 5;
+    let playerInitialCostString = document.getElementById('playerTotalCost').innerText;
+    const playerInitialCost = parseFloat(playerInitialCostString);
+    const finalCost = totalPlayerCost + playerInitialCost;
+    playerInitialCostString.innerText = finalCost;
+    console.log(finalCost);
+})
