7aaa73b5d08ac2ca3b1c83bf19a7c8fec21308cf
dev-nayandas <rich.nayan@gmail.com>
2022-08-22 04:03:56 +0600

    calculation done

--- calculation.js
+++ calculation.js
@@ -23,13 +23,23 @@ document.getElementById('finalCost').addEventListener('click', function() {
 
     
 
-
+    // const costString = getInputFieldValueById('perPlayerCost');
+    // const costAmount = parseFloat(costString) * 5;
+    // console.log(costAmount);
+    // const costTotalElement = document.getElementById('playerTotalCost');
+    // const costTotalString = costTotalElement.innerText;
+    // const previousCostTotal = parseFloat(costTotalString);
+    // const currentCostTotal =  costAmount;
+    // costTotalElement.innerText = currentCostTotal;
+    const playerCostString = document.getElementById('playerTotalCost').innerText;
+    const playerCostAmount = parseFloat(playerCostString);
     const managerCost = getInputFieldValueById('managerCost');
     const coachCost = getInputFieldValueById('coachCost');
     const finalCostElement = document.getElementById('playerFinalCost');
     const costFinalString = finalCostElement.innerText;
     const previousCostFinal = parseFloat(costFinalString);
-    const currentCostFinal =  managerCost + coachCost + costAmount;
+    const currentCostFinal =  managerCost + coachCost + playerCostAmount;
     finalCostElement.innerText = currentCostFinal;
+    console.log(currentCostFinal)
    
 })
