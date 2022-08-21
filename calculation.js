7e191f51f5f57b92d4207915e196ac91afd7083e
dev-nayandas <rich.nayan@gmail.com>
2022-08-22 03:35:21 +0600

    some issue solved

--- calculation.js
+++ calculation.js
@@ -1,23 +1,35 @@
-// function getInputFieldValueById(inputFieldId) {
-//     const inputField = document.getElementById(inputFieldId);
-//     const inputFieldValueString = inputField.value;
-//     const inputFieldValue = parseFloat(inputFieldValueString);
-//     inputField.value = '';
-//     return inputFieldValue;
-// }
-
-
-
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
 
 document.getElementById('calculationButton').addEventListener('click', function(){
+  
+    // const costField = document.getElementById('perPlayerCost');
+    // const costString = costField.value;
+    const costString = getInputFieldValueById('perPlayerCost');
+    const costAmount = parseFloat(costString) * 5;
+    const costTotalElement = document.getElementById('playerTotalCost');
+    const costTotalString = costTotalElement.innerText;
+    const previousCostTotal = parseFloat(costTotalString);
+    const currentCostTotal =  costAmount;
+    costTotalElement.innerText = currentCostTotal;
+});
 
+document.getElementById('finalCost').addEventListener('click', function() {
+
+    
+
+
+    const managerCost = getInputFieldValueById('managerCost');
+    const coachCost = getInputFieldValueById('coachCost');
+    const finalCostElement = document.getElementById('playerFinalCost');
+    const costFinalString = finalCostElement.innerText;
+    const previousCostFinal = parseFloat(costFinalString);
+    const currentCostFinal =  managerCost + coachCost + costAmount;
+    finalCostElement.innerText = currentCostFinal;
    
-    const perPlayerCost = document.getElementById('perPlayerCost').value;
-    const perPlayerCostValue = parseFloat(perPlayerCost);
-    const totalPlayerCost = perPlayerCostValue * 5;
-    let playerInitialCostString = document.getElementById('playerTotalCost').innerText;
-    const playerInitialCost = parseFloat(playerInitialCostString);
-    const finalCost = totalPlayerCost + playerInitialCost;
-    playerInitialCostString.innerText = finalCost;
-    console.log(finalCost);
 })
